d9d50e679b1d1599f0069929dc494282bbaebd1e
Your Name <you@example.com>
2022-10-25 21:55:42 +0200

    Updates

---
+++ precache-manifest.0b26c671c9a4d39ab08b1a992916fcc8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "209302c8e5e37fe91129",
+    "url": "/Meme_generator/static/css/main.309ac1f1.chunk.css"
+  },
+  {
+    "revision": "209302c8e5e37fe91129",
+    "url": "/Meme_generator/static/js/main.209302c8.chunk.js"
+  },
+  {
+    "revision": "ba38ca85c16333d26198",
+    "url": "/Meme_generator/static/js/1.ba38ca85.chunk.js"
+  },
+  {
+    "revision": "0759aa17cf76189dcab3",
+    "url": "/Meme_generator/static/js/runtime~main.0759aa17.js"
+  },
+  {
+    "revision": "054b056198d35cfe69dee35513169dac",
+    "url": "/Meme_generator/index.html"
+  }
+];
